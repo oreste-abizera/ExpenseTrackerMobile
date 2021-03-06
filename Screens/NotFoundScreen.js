41be54db4eea3a428488773c328dc0e2f4e12c62
oreste-abizera <oresteabizera11@gmail.com>
2021-03-06 21:32:27 +0200

    Adding some styles on NotFound screen

--- Screens/NotFoundScreen.js
+++ Screens/NotFoundScreen.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import Context from "../Context/ContextProvider";
 
 export default function NotFoundScreen() {
+  const { changeNavigation } = React.useContext(Context);
   return (
-    <View>
-      <Text>Screen Not Found</Text>
+    <View
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "40%",
+      }}
+    >
+      <Text>Screen is under development</Text>
+      <TouchableOpacity
+        style={{
+          marginTop: 30,
+          backgroundColor: "rgb(32, 137, 220)",
+          padding: 10,
+          borderRadius: 10,
+        }}
+        onPress={() => changeNavigation("Home")}
+      >
+        <Text style={{ color: "#fff" }}>Go back to Home</Text>
+      </TouchableOpacity>
     </View>
   );
 }
